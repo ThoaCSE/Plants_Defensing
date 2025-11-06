da0ada0c9b2380f9c866d6f2394c4d2e6730c9e0
ThoaCSE <nguyenhuynhkthoa@gmail.com>
2025-11-06 14:57:48 +0700

    Game loop skeleton with enscapsulation

--- src/plantsdefense/GameFrame.java
+++ src/plantsdefense/GameFrame.java
@@ -1,7 +1,22 @@
 package plantsdefense;
 
+import plantsdefense.util.Constants;
 import javax.swing.*;
 import java.awt.*;
 
 public class GameFrame extends JFrame {
+    public GameFrame(){
+        setTitle("Plants Defense - CSE2023_VGU");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+
+        add(new GamePanel());
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args){
+        SwingUtilities.invokeLater(GameFrame::new);
+    }
 }
