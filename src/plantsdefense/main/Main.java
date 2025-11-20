c5cde6b657607a63921b13bc4da63cf7215304bb
ThoaCSE <nguyenhuynhkthoa@gmail.com>
2025-11-20 20:51:32 +0700

    Split the main function

--- src/plantsdefense/main/Main.java
+++ src/plantsdefense/main/Main.java
@@ -1,4 +1,9 @@
 package plantsdefense.main;
 
+import plantsdefense.gui.GameFrame;
+
 public class Main {
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(GameFrame::new);
+    }
 }
