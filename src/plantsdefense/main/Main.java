1af34b3e40d5817d897b5564593b64dab7888c63
anhthu-tat <10423173@student.vgu.edu.vn>
2025-11-25 10:52:08 +0700

    played

--- src/plantsdefense/main/Main.java
+++ src/plantsdefense/main/Main.java
@@ -1,9 +1,13 @@
 package plantsdefense.main;
 
 import plantsdefense.gui.GameFrame;
+import plantsdefense.jdbc.DatabaseInitializer;
 
 public class Main {
     public static void main(String[] args) {
+        // This runs ONCE — creates tables + inserts all your .txt levels
+        DatabaseInitializer.init();
+
         javax.swing.SwingUtilities.invokeLater(GameFrame::new);
     }
 }
