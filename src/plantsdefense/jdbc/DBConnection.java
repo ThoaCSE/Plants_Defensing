9123c218cf8bcd692b2ccdea1abdc9931bbe35d7
anhthu-tat <10423173@student.vgu.edu.vn>
2025-11-25 21:39:26 +0700

    DatabaseConnection code ver 1

--- src/plantsdefense/jdbc/DBConnection.java
+++ src/plantsdefense/jdbc/DBConnection.java
@@ -1,4 +1,31 @@
 package plantsdefense.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/plantsdefensed?useSSL=false&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASS = "root1234";  // Your password
+
+    private static Connection connection = null;
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection(URL, USER, PASS);
+        }
+        return connection;
+    }
+
+    public static void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
+
